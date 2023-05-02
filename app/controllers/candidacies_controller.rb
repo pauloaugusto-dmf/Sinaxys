3769ae76362d7210a42a3509f1460015248da035
paulo <pauloaugusto@id.uff.br>
2023-05-02 09:10:10 -0300

    FIX - alert message

--- app/controllers/candidacies_controller.rb
+++ app/controllers/candidacies_controller.rb
@@ -6,8 +6,7 @@ class CandidaciesController < ApplicationController
     if @candidacy.save
       redirect_to root_url, notice: t('controller.notice.candidacy_was_successfully_created')
     else
-      flash[:alert] = @candidacy.errors.full_messages
-      redirect_to root_url
+      redirect_to root_url, alert: @candidacy.errors.full_messages
     end
   end
 
@@ -20,7 +19,6 @@ class CandidaciesController < ApplicationController
   def require_login
     return if current_user
 
-    flash[:error] = t('controller.error.you_must_be_logged_in_to_apply_for_a_job')
-    redirect_to new_user_session_path
+    redirect_to new_user_session_path, alert: t('controller.error.you_must_be_logged_in_to_apply_for_a_job')
   end
 end
