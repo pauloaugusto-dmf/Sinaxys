117cdac6e139a8be61e4f5749332e57acb12c055
paulo <pauloaugusto@id.uff.br>
2023-05-02 08:07:18 -0300

    adding error handling for services vacancies

--- app/services/vacancies/list.rb
+++ app/services/vacancies/list.rb
@@ -10,6 +10,12 @@ module Vacancies
     def call
       response = RestClient.get(@api_url)
       JSON.parse(response.body)
+    rescue RestClient::ExceptionWithResponse => e
+      Rails.logger.error("API request failed: #{e.response}")
+      nil
+    rescue StandardError => e
+      Rails.logger.error("Failed with error: #{e.message}")
+      nil
     end
   end
 end
